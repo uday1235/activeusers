3e1d515f9cd0249da852fdda476a793828b8098c
Uday <udayshankar666@gmail.com>
2024-01-23 11:26:15 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACzfDWY8j74msWjVOUzkIE2ZN8r6_JujY",
-  authDomain: "phoneit-ef566.firebaseapp.com",
-  projectId: "phoneit-ef566",
-  storageBucket: "phoneit-ef566.appspot.com",
-  messagingSenderId: "354413843488",
-  appId: "1:354413843488:web:565e15586e08797bf85845"
+  //firebase config
 };
 
 const app = initializeApp(firebaseConfig);
